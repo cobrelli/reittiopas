a5fe5956f108e51a22d87e38e036eda5b4a5f58d
cobrelli <cobrelli@gmail.com>
2013-09-15 17:15:01 +0300

    reittiopas 2 lisays

---
+++ src/reittiopas/Reittiopas2.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reittiopas;
+
+/**
+ *
+ * @author Cobrelli
+ */
+public class Reittiopas2 {
+    
+}
